9ccf98f9f512b569a4526fa3349b0599447836cf
Beautyfly <he2013fei@126.com>
2015-05-20 13:24:50 +0800

    add

--- shell/AddUsers/addUsers.sh
+++ shell/AddUsers/addUsers.sh
@@ -1,4 +1,10 @@
-！/bin/bash
-echo "hello world!"
-echo "I'm having breakfast~~~"
-echo date
+#!/bin/bash
+#chmod 700 addUsers.sh
+#./addUsers.sh
+for user in `cat /home/beaityfly/Beautyflying/Linux101-assignment/shell/AddUsers/users.txt`
+do
+useradd -m $user
+echo "$user:redhat" | chpasswd
+echo "$user create success!"
+done
+pwconv
